df281a76151acfe4b74997e89dc8ba1081001f65
Daryl <darylnauman@gmail.com>
2022-05-13 16:01:42 -0400

    Add ability to add user, delete user and getAllUsers

--- recipe-api/src/main/java/com/ex/recipeapi/services/UserService.java
+++ recipe-api/src/main/java/com/ex/recipeapi/services/UserService.java
@@ -1,9 +1,13 @@
 package com.ex.recipeapi.services;
 
+import com.ex.recipeapi.entities.User;
+import com.ex.recipeapi.exceptions.UserNotFoundException;
 import com.ex.recipeapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -12,4 +16,24 @@ public class UserService {
     @Autowired
     private UserRepository users;
 
+    public List<User> getAllUsers() {
+        return users.findAll();
+    }
+
+    public User addUser(User user) {
+        return users.save(user);
+    }
+
+    public boolean deleteUser(Integer userId) {
+        if (users.existsById(userId)) {
+            users.deleteById(userId);
+            if (!users.existsById(userId)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            throw new UserNotFoundException("No user with this user id found to delete");
+        }
+    }
 }
